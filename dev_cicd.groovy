42bd9f384a8090898fb69bedefbf4abc41e72c34
jayanthikr91 <jayanthikr91@gmail.com>
2019-01-17 18:57:02 +0530

    Update dev_cicd.groovy

--- dev_cicd.groovy
+++ dev_cicd.groovy
@@ -4,28 +4,27 @@ def pom
 stage('checkout')
 
 {
-echo "in checkout"
-echo "${prop.GIT_URL}"
+
 def giturl=prop.GIT_URL
 echo "giturl=${giturl}"
-git "${giturl}"
+git prop.GIT_URL
 pom=readMavenPom  file: prop.POM_FILE
 }
 
 stage ('build'){
 
 echo "${prop.MAVEN_CMD}"
-sh "${prop.MAVEN_CMD}"
+sh prop.MAVEN_CMD
 
 }
 
 stage('code analysis'){
     echo "${prop.MAVEN_SONAR_CMD}+${prop.SONAR_URL}"
-    sh "${prop.MAVEN_SONAR_CMD}+${prop.SONAR_URL}"
+    sh prop.MAVEN_SONAR_CMD +prop.SONAR_URL
 }
 stage('Artifactory upload')
 {
-def server = Artifactory.server "${prop.ARTIFACT_ID}"
+def server = Artifactory.server prop.ARTIFACT_ID
 def uploadSpec = """{
  	
 "files":[
@@ -39,15 +38,15 @@ server.upload(uploadSpec)
 }
 stage ('Final deploy'){
 echo "${prop.SECURE_COPY_CMD}+${prop.SRC_DEPLOY_LOC}+${prop.DEST_DEPLOY_LOC}"
-sh "${prop.SECURE_COPY_CMD}+${prop.SRC_DEPLOY_LOC}+${prop.DEST_DEPLOY_LOC}"
+sh prop.SECURE_COPY_CMD+prop.SRC_DEPLOY_LOC+prop.DEST_DEPLOY_LOC
 }
 stage('success email'){
-    emailext body: 'Build is successful', subject: 'email notification', to: 'Jayanthi.Ravinathan@mindtree.com'
+    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
   }
   }
   catch(Exception e){
   stage('failure email'){
-    emailext body: 'Build is unsuccessful', subject: 'email notification', to: 'Jayanthi.Ravinathan@mindtree.com'
+    emailext body: '${DEFAULT_CONTENT}', subject: '${DEFAULT_SUBJECT}', to: prop.RECEPIENT_MAIL_ID
   }
   }
 
